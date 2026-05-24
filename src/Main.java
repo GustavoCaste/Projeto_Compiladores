a38b7fe19a1e1d486318dd07d8d70f2f07db5240
gustavo <gustavocastelluccio.es@gmail.com>
2026-05-24 15:57:11 -0300

    Feat: Add initial example program MeuPrograma and enhance error handling in Main

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,7 @@ public class Main {
             System.out.println(" - " + tabPath);
         } catch (Exception e) {
             System.err.println("Erro ao executar o fluxo inicial: " + e.getMessage());
+            System.exit(1);
         }
     }
 
