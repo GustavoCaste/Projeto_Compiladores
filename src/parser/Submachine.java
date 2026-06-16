8de0c9b3c5ec4d38cb0305ab16332dee4367a7d4
Leobritt <leobritto160@gmail.com>
2026-06-16 17:35:00 -0300

    feat: Add Submachine enum for syntactic analysis in parser

---
+++ src/parser/Submachine.java
@@ -0,0 +1,9 @@
+package parser;
+
+// Submaquinas previstas para o analisador sintatico do Klebao2026-1.
+public enum Submachine {
+    SUB01_FILE_PROGRAM,
+    SUB02_COMMAND,
+    SUB03_LOGICAL_EXP,
+    SUB04_ADD_EXP
+}
